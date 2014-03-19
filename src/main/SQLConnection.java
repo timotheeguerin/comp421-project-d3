b16d1cbf16e54ef9bf910ae4674ee5ee99abb58b
Timothee Guerin <timothee.guerin@outlook.com>
2014-03-19 13:44:08 -0400

    convert to hash

--- src/main/SQLConnection.java
+++ src/main/SQLConnection.java
@@ -1,29 +1,34 @@
 package main;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SQLConnection {
-	//bendodev.no-ip.org:3306/comp421project?" + "user=root&password=madremia350");
+	// bendodev.no-ip.org:3306/comp421project?" + "user=root&password=madremia350");
 	private final String MYSQL_DRIVER_CLASS = "org.gjt.mm.mysql.Driver";
-	
+
 	public Connection db;
-	
-	public SQLConnection(String url,String dbName, String user, String password) {
+
+	public SQLConnection(String url, String dbName, String user, String password) {
 		try {
 			Class.forName(MYSQL_DRIVER_CLASS);
 		} catch (ClassNotFoundException e) {
 			System.out.println("Class: " + MYSQL_DRIVER_CLASS + " not found.");
 			e.printStackTrace();
 		}
-		
+
 		try {
-			db = DriverManager.getConnection("jdbc:mysql://" + url + "/"+ dbName + "?" +  "user=" + user + "&password=" + password);
+			db = DriverManager.getConnection("jdbc:mysql://" + url + "/"
+					+ dbName + "?" + "user=" + user + "&password=" + password);
 		} catch (SQLException e) {
 			System.out.println("Couldn't connect to " + url + ".");
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void CloseConnection() {
 		try {
 			db.close();
@@ -32,22 +37,38 @@ public class SQLConnection {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public ResultSet ExecuteQuery(String query, String... args) {
 		try {
 			PreparedStatement st = db.prepareStatement(query);
-			for(int i = 0;i<args.length;i++){
-				st.setString(i+1, args[i]);
+			for (int i = 0; i < args.length; i++) {
+				st.setString(i + 1, args[i]);
 			}
 			System.out.println(st.toString());
 			ResultSet rs = st.executeQuery();
 			return rs;
-		} catch(SQLException e){
+		} catch (SQLException e) {
 			System.out.println("Couldn't execute query");
 			e.printStackTrace();
 			return null;
 		}
 	}
 
-	
+	private List<HashMap<String, String>> resultSetToHashList(ResultSet rs) {
+		List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+		try {
+			ResultSetMetaData rsmd = rs.getMetaData();
+			while (rs.next()) {
+				HashMap<String, String> map = new HashMap<String, String>();
+				for (int i = 0; i < rsmd.getColumnCount(); i++) {
+					String colName = rsmd.getColumnName(i);
+					map.put(colName, rs.getString(colName));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
 }
